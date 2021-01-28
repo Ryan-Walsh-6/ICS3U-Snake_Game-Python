4373d351eee46ee4e47b902e39d02f2a7fa17373
Ryan-Walsh-6 <74618988+Ryan-Walsh-6@users.noreply.github.com>
2021-01-27 20:44:11 -0500

    Create constants.py

---
+++ Lesson 4/constants.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Ryan Walsh
+# Created on: December 2020
+# This program is the constants file for The Snake Game game
+
+# PyBadge screen size is 160x128 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+FPS = 60
