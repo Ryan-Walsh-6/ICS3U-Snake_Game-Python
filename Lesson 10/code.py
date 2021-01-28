2a6111c24829a71811e8cec27643962d31713e6e
Ryan-Walsh-6 <74618988+Ryan-Walsh-6@users.noreply.github.com>
2021-01-28 17:34:34 -0500

    Update code.py

--- Lesson 10/code.py
+++ Lesson 10/code.py
@@ -6,13 +6,12 @@
 
 import ugame
 import stage
-import random
 import time
+import random
 import neopixel
 import board
-import supervisor
-
 import constants
+
 
 def splash_scene():
     # this function is the splash scene
@@ -127,19 +126,19 @@ def menu_scene():
 def game_scene():
    # this function is the main game game_scene
    def show_apple():
+       #this function places the apple at a random location
        apple.move(random.randint(0 + 
-               constants.SNAKE_SIZE,constants.SCREEN_X - 
-               constants.SNAKE_SIZE),(random.randint(0 + 
-               constants.SNAKE_SIZE,constants.SCREEN_Y - 
-               constants.SNAKE_SIZE)))
-    
+               constants.SPRITE_SIZE,constants.SCREEN_X - 
+               constants.SPRITE_SIZE),(random.randint(0 + 
+               constants.SPRITE_SIZE,constants.SCREEN_Y - 
+               constants.SPRITE_SIZE)))
+               
    def show_snake(x , y):
         # this function takes a snake from off screen and moves it on screen
        for snake_number in range(len(snakes)):
            if snakes[snake_number].x < 0:
                snakes[snake_number].move(x,y)
                break
-   
    # get sound ready
    apple_crunch = open("apple_crunch.wav", 'rb')
    sound = ugame.audio
@@ -148,23 +147,16 @@ def game_scene():
    
    neopixels = neopixel.NeoPixel(board.NEOPIXEL, constants.NEOPIXEL_COUNT, 
                                  auto_write=False)
-   # for score
-   score = 0
 
-   score_text = stage.Text(width=29, height=14)
-   score_text.clear()
-   score_text.cursor(0,0)
-   score_text.move(1,1)
-   score_text.text("Score: {0}".format(score))
-    
    # image banks for CircuitPython
    image_bank_bankground = stage.Bank.from_bmp16("ball.bmp")
    image_bank_sprites = stage.Bank.from_bmp16("PyBadge_bank_color_template.bmp")
    
    # set the background to image 0 in the image Bank
    #   and the size (10x8 tiles of the size 16x16)
-   background = stage.Grid(image_bank_bankground, 10,8)
+   background = stage.Grid(image_bank_bankground, constants.SCREEN_GRID_X ,constants.SCREEN_GRID_Y)
    
+   # a sprite that will be updated every frame
    # a snake sprite that will be updated every frame
    snakes = []
    for snake_number in range(constants.MAX_SCORE):
@@ -175,36 +167,32 @@ def game_scene():
    x = constants.SCREEN_X/2
    y = constants.SCREEN_Y/2
    show_snake(x, y)
-
    # an apple sprite that will be updated every frame
    apple = stage.Sprite(image_bank_sprites, 8 , constants.OFF_SCREEN_X,
                                      constants.OFF_SCREEN_Y)
    #place apple on the screen
-   apple.move(constants.SCREEN_X/2 + 32,constants.SCREEN_Y/2 -32 ) 
- 
-   # create a stage for the background to show up on
+   apple.move(constants.SCREEN_X/2 + constants.APPLE_OFFSET,constants.SCREEN_Y/2 -constants.APPLE_OFFSET) 
+    # create a stage for the background to show up on
    #   and set the frame rate for 5fps
-   game = stage.Stage(ugame.display, 5)
+   game = stage.Stage(ugame.display, constants.GAME_FPS)
    # set layers of all sprites, items show up in order
-   game.layers = [score_text] + snakes + [apple] + [background]
+   game.layers = snakes + [apple] + [background]
    # render all sprites
    #   most likely you will only render the background once per game scene
    game.render_block()
+   
    #define initial values for the main loop
    right_button = constants.button_state["button_up"]
    left_button = constants.button_state["button_up"]
    up_button = constants.button_state["button_up"]
    down_button = constants.button_state["button_up"]
-   HEAD = 0
-   wormCoords = [{'x': snakes[HEAD].x , 'y': snakes[HEAD].y}]
-   newHead = {'x':snakes[HEAD].x, 'y' :snakes[HEAD].y}
-   collision = False
-
-   # repeat forever, game loop
+   wormCoords = [{'x': snakes[constants.HEAD_INDEX].x , 'y': snakes[constants.HEAD_INDEX].y}]
+   newHead = {'x':snakes[constants.HEAD_INDEX].x, 'y' :snakes[constants.HEAD_INDEX].y}
+    # repeat forever, game loop
    while True:
        # get user input
        keys = ugame.buttons.get_pressed()
-      
+       
        if keys & ugame.K_RIGHT !=0:
            right_button = constants.button_state["button_just_pressed"]
            left_button = constants.button_state["button_up"]
@@ -225,7 +213,7 @@ def game_scene():
            left_button = constants.button_state["button_up"]
            up_button = constants.button_state["button_up"]
            down_button = constants.button_state["button_just_pressed"]
-
+       # update game logic
        if keys & ugame.K_X:
            pass
        if keys & ugame.K_O:
@@ -234,15 +222,17 @@ def game_scene():
            pass
        if keys & ugame.K_SELECT:
            pass
-        #set neopixels to blue by default
+     
        neopixels[0] = (0, 0, 10)
        neopixels[1] = (0, 0, 10)
        neopixels[2] = (0, 0, 10)
        neopixels[3] = (0, 0, 10)
        neopixels[4] = (0, 0, 10) 
-      
-       if stage.collide(snakes[HEAD].x , snakes[HEAD].y,
-                        snakes[HEAD].x + 15, snakes[HEAD].y + 15, 
+       
+       
+       #Check for apple and snake collision
+       if stage.collide(snakes[constants.HEAD_INDEX].x , snakes[constants.HEAD_INDEX].y,
+                        snakes[constants.HEAD_INDEX].x + 15, snakes[constants.HEAD_INDEX].y + 15, 
                         apple.x, apple.y,
                         apple.x + 15, apple.y + 15):
            neopixels[0] = (10, 10, 0)
@@ -261,223 +251,44 @@ def game_scene():
    
            sound.stop()
            sound.play(apple_crunch)
-           # you hit an apple
-           score = score + 1
-           score_text.clear()
-           score_text.cursor(0,0)
-           score_text.move(1,1)
-           score_text.text("Score: {0}".format(score))
-
            apple.move(constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
            #place apple in new location
            show_apple()
            if right_button == constants.button_state["button_just_pressed"] :
-               newHead = {'x':snakes[HEAD].x +16, 'y' :snakes[HEAD].y}
+                newHead = {'x':snakes[constants.HEAD_INDEX].x + constants.SPRITE_SIZE, 'y' :snakes[constants.HEAD_INDEX].y}
            elif left_button == constants.button_state["button_just_pressed"] :
-               newHead = {'x':snakes[HEAD].x - 16, 'y' :snakes[HEAD].y}
+                newHead = {'x':snakes[constants.HEAD_INDEX].x - constants.SPRITE_SIZE, 'y' :snakes[constants.HEAD_INDEX].y}
            elif up_button == constants.button_state["button_just_pressed"] :
-               newHead = {'x':snakes[HEAD].x, 'y' :snakes[HEAD].y - 16}
+                newHead = {'x':snakes[constants.HEAD_INDEX].x, 'y' :snakes[constants.HEAD_INDEX].y - constants.SPRITE_SIZE}
            elif down_button == constants.button_state["button_just_pressed"] :
-               newHead = {'x':snakes[HEAD].x, 'y' :snakes[HEAD].y + 16}
+                newHead = {'x':snakes[constants.HEAD_INDEX].x, 'y' :snakes[constants.HEAD_INDEX].y + constants.SPRITE_SIZE}
            wormCoords.insert(0,newHead)
        else:        
            if right_button == constants.button_state["button_just_pressed"] :
-               newHead = {'x':snakes[HEAD].x +16, 'y' :snakes[HEAD].y}
+               newHead = {'x':snakes[constants.HEAD_INDEX].x +constants.SPRITE_SIZE, 'y' :snakes[constants.HEAD_INDEX].y}
            elif left_button == constants.button_state["button_just_pressed"] :
-               newHead = {'x':snakes[HEAD].x - 16, 'y' :snakes[HEAD].y}
+               newHead = {'x':snakes[constants.HEAD_INDEX].x - constants.SPRITE_SIZE, 'y' :snakes[constants.HEAD_INDEX].y}
            elif up_button == constants.button_state["button_just_pressed"] :
-               newHead = {'x':snakes[HEAD].x, 'y' :snakes[HEAD].y - 16}
+               newHead = {'x':snakes[constants.HEAD_INDEX].x, 'y' :snakes[constants.HEAD_INDEX].y - constants.SPRITE_SIZE}
            elif down_button == constants.button_state["button_just_pressed"] :
-               newHead = {'x':snakes[HEAD].x, 'y' :snakes[HEAD].y + 16}
+               newHead = {'x':snakes[constants.HEAD_INDEX].x, 'y' :snakes[constants.HEAD_INDEX].y + constants.SPRITE_SIZE}
            wormCoords.insert(0,newHead)
            del wormCoords[-1]
-       
-       #Move all snakes off screen
+           
+       #Move all snakes off screen   
        for snake_number in range (len(snakes)):
-           snakes[snake_number].move(constants.OFF_SCREEN_X,
-                                     constants.OFF_SCREEN_Y)
+           snakes[snake_number].move(constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
+           
        #Re-draw snake
        for coords in wormCoords:
            x= coords['x']
            y= coords['y']
            show_snake(x,y)
-
-       #Check for off-screen
-       if snakes[HEAD].x > (constants.SCREEN_X - constants.SNAKE_SIZE):
-            collision = True
-       for coords in wormCoords:
-           x= coords['x']
-           if x < 0 :
-                collision = True
-       if snakes[HEAD].y <  0:
-                collision = True
-       if snakes[HEAD].y > (constants.SCREEN_Y - constants.SNAKE_SIZE):
-                collision = True
-       
-
-       #Check for the snake collision with itself
-       for snake_number in range (1,len(snakes)):
-           if snakes[snake_number].x > 0:
-                       if stage.collide(snakes[HEAD].x , 
-                                        snakes[HEAD].y,
-                                        snakes[HEAD].x + 15,
-                                        snakes[HEAD].y + 15,
-                                        snakes[snake_number].x ,
-                                        snakes[snake_number].y,
-                                        snakes[snake_number].x + 15,
-                                        snakes[snake_number].y + 15):
-                           collision = True
-       
-       if collision == True:
-           neopixels[0] = (10, 0, 0)
-           neopixels[1] = (10, 0, 0)
-           neopixels[2] = (10, 0, 0)
-           neopixels[3] = (10, 0, 0)
-           neopixels[4] = (10, 0, 0)
-           neopixels.show()
-           time.sleep(0.5)
-           you_lost_scene(score)      
-
-       if score == constants.MAX_SCORE:
-           for i in range(score):
-               neopixels[0] = (0, 10, 0)
-               neopixels[1] = (0, 10, 0)
-               neopixels[2] = (0, 10, 0)
-               neopixels[3] = (0, 10, 0)
-               neopixels[4] = (0, 10, 0)
-               neopixels.show()
-               time.sleep(0.1)
-               neopixels[0] = (0, 0, 0)
-               neopixels[1] = (0, 0, 0)
-               neopixels[2] = (0, 0, 0)
-               neopixels[3] = (0, 0, 0)
-               neopixels[4] = (0, 0, 0)
-               neopixels.show()
-               time.sleep(0.1)
-           neopixels[0] = (0, 10, 0)
-           neopixels[1] = (0, 10, 0)
-           neopixels[2] = (0, 10, 0)
-           neopixels[3] = (0, 10, 0)
-           neopixels[4] = (0, 10, 0)
-           neopixels.show()
-           time.sleep(0.5)
-           you_won_scene(score)
+           
        # redraw Sprite
        neopixels.show()
        game.render_sprites(snakes + [apple])
        game.tick() # wait until refresh rate finishes
-
-def you_won_scene(score):
-    # turn off sound from last scene
-   sound = ugame.audio
-   sound.stop()
-   # image banks for Circuitpython
-   image_bank_2 = stage.Bank.from_bmp16("mt_game_studio.bmp")
-           
-   # sets the background to image 0 in the image bank
-   background = stage.Grid(image_bank_2, constants.SCREEN_GRID_X,
-                                    constants.SCREEN_GRID_Y)
-    
-   text = []
-   text1 = stage.Text(width=29, height=14, font=None,
-   palette=constants.BLUE_PALETTE, buffer=None)
-   text1.move(43,60)
-   text1.text("YOU WIN!")
-   text.append(text1)
-
-   # create a stage for the background to show up on
-   #     ad set the frame rate to 60fps
-   game = stage.Stage(ugame.display, constants.FPS)
-   # set layers of all sprites, items show up in order
-   game.layers = text + [background]
-   # render all sprites
-   #   most likely you will only render the background once per game scene
-   game.render_block()
-   time.sleep(3.0)
-   game_over_scene(score)
-
-def you_lost_scene(score):
-    # turn off sound from last scene
-   sound = ugame.audio
-   sound.stop()
-   # image banks for Circuitpython
-   image_bank_2 = stage.Bank.from_bmp16("mt_game_studio.bmp")
-           
-   # sets the background to image 0 in the image bank
-   background = stage.Grid(image_bank_2, constants.SCREEN_GRID_X,
-                                    constants.SCREEN_GRID_Y)
-       
-   text = []
-   text1 = stage.Text(width=29, height=14, font=None,
-   palette=constants.BLUE_PALETTE, buffer=None)
-   text1.move(43,60)
-   text1.text("YOU LOSE :(")
-   text.append(text1)
-
-   # create a stage for the background to show up on
-   #     ad set the frame rate to 60fps
-   game = stage.Stage(ugame.display, constants.FPS)
-   # set layers of all sprites, items show up in order
-   game.layers = text + [background]
-   # render all sprites
-   #   most likely you will only render the background once per game scene
-   game.render_block()
-   time.sleep(3.0)
-   game_over_scene(score)
-
-def game_over_scene(final_score):
-    # this function is the game over scene
-           
-   # turn off sound from last scene
-   sound = ugame.audio
-   sound.stop()
-           
-   # image banks for Circuitpython
-   image_bank_2 = stage.Bank.from_bmp16("mt_game_studio.bmp")
-           
-   # sets the background to image 0 in the image bank
-   background = stage.Grid(image_bank_2, constants.SCREEN_GRID_X,
-                                    constants.SCREEN_GRID_Y)
-            
-   # add text objects
-   text = []
-   text1 = stage.Text(width=29, height=14, font=None,
-   palette=constants.BLUE_PALETTE, buffer=None)
-   text1.move(22,20)
-   text1.text("Final Score: {:0>2d}".format(final_score))
-   text.append(text1)
-           
-   text2 = stage.Text(width=29, height=14, font=None, 
-                      palette=constants.BLUE_PALETTE, buffer=None) 
-   text2.move(43,60)
-   text2.text("GAME OVER")
-   text.append(text2)
-           
-   text3 = stage.Text(width=29, height=14, font=None, 
-                      palette=constants.BLUE_PALETTE, buffer=None)
-   text3.move(32,110)
-   text3.text("PRESS SELECT")
-   text.append(text3)
-   # create a stage for the background to show up on
-   #     ad set the frame rate to 60fps
-   game = stage.Stage(ugame.display, constants.FPS)
-   # set layers of all sprites, items show up in order
-   game.layers = text + [background]
-   # render all sprites
-   #   most likely you will only render the background once per game scene
-   game.render_block()
-           
-   # repeat forever, game loop
-   while True:
-       # get user input
-       keys = ugame.buttons.get_pressed()
-                
-       # Start button selecte
-       if keys & ugame.K_SELECT !=0:
-           supervisor.reload()
-                
-           # update game logic
-       game.tick() #  wait until refresh rate finishes
 
 if __name__ == "__main__":
     splash_scene()
